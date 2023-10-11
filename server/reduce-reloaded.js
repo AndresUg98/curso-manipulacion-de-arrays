f697729a33e24801e2c428bfcca009d2b5a5f7a5
AndresUg98 <oscaran2109@gmail.com>
2023-10-11 13:52:11 -0600

    trying to solve de challenge

--- server/reduce-reloaded.js
+++ server/reduce-reloaded.js
@@ -55,3 +55,22 @@ const rta2 = data
   }, {});
 
 console.log(rta2);
+
+//Contar por rangos
+//Cuantos valores hay de 1 - 5
+//Cuantos valores hay de 6 - 8
+//Cuantos valores hay de 9 - 10
+const items2 = [1, 3, 2, 3];
+const rta3 = items2
+  .map((item) => item.level)
+  .reduce((obj, item) => {
+    if (!obj[item]) {
+      obj[item] = 1;
+    } else {
+      obj[item] = obj[item] + 1;
+    }
+
+    return obj;
+  }, {});
+
+console.log(rta3);
