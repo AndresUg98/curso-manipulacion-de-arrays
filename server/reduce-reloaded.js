98c762f7e18c8c347ad0f029ddb7fe0c673b0bd3
AndresUg98 <oscaran2109@gmail.com>
2023-10-11 13:51:56 -0600

    trying to solve de challenge

---
+++ server/reduce-reloaded.js
@@ -0,0 +1,57 @@
+const items = [1, 3, 2, 3];
+
+//El estado inicial en este caso sera un objeto en vacio
+const rta = items.reduce((obj, item) => {
+  if (!obj[item]) {
+    obj[item] = 1;
+  } else {
+    obj[item] = obj[item] + 1;
+  }
+
+  return obj;
+}, {});
+
+console.log(rta);
+
+const data = [
+  {
+    name: "Nicolas",
+    level: "low",
+  },
+  {
+    name: "Andrea",
+    level: "medium",
+  },
+  {
+    name: "Zulema",
+    level: "hight",
+  },
+  {
+    name: "Santiago",
+    level: "low",
+  },
+  {
+    name: "Valentina",
+    level: "medium",
+  },
+  {
+    name: "Lucia",
+    level: "hight",
+  },
+];
+
+//Saber cuantas personas tienen el nivel
+
+const rta2 = data
+  .map((item) => item.level)
+  .reduce((obj, item) => {
+    if (!obj[item]) {
+      obj[item] = 1;
+    } else {
+      obj[item] = obj[item] + 1;
+    }
+
+    return obj;
+  }, {});
+
+console.log(rta2);
--- server/reduce.js
+++ server/reduce.js
@@ -1,4 +1,4 @@
-const totals = [1,2,3,4];
+const totals = [1, 2, 3, 4];
 
 //El primer atributo es el que acumula, en el que estara el resultado final
 //el segundo atributo el el elemento o item que se esta iterando
@@ -6,6 +6,11 @@ const totals = [1,2,3,4];
 //El primer parametro como tal seria toda la arrow function
 //El segundo parametro seria el estado inicial de este caso 'sum'
 
-const rta = totals.reduce((sum,item)=> sum + item,0);
+const rta = totals.reduce((sum, item) => sum + item, 0);
 
 console.log(rta);
+
+//Tienes un array de números y tu reto es retornar la suma de todos los valores en él.
+function calcSum(numbers) {
+  return numbers.reduce((sum, item) => sum + item, 0);
+}
