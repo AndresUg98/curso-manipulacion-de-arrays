a52de0b3e87cecb5c0b7ab976420f022322496ae
AndresUg98 <oscaran2109@gmail.com>
2023-10-13 13:56:05 -0600

    flat y flatMap

---
+++ server/every.js
@@ -0,0 +1,24 @@
+const numbers = [1, 30, 49, 29, 10, 13];
+
+const rta = numbers.every((item) => item <= 40);
+
+console.log(rta);
+
+const team = [
+  {
+    name: "Nicolas",
+    age: 12,
+  },
+  {
+    name: "Andrea",
+    age: 8,
+  },
+  {
+    name: "Zulema",
+    age: 2,
+  },
+  {
+    name: "Santiago",
+    age: 18,
+  },
+];
---
+++ server/flat.js
@@ -0,0 +1,9 @@
+const matriz = [
+  [1, 2, 3],
+  [1, 2, 3, [1, 3]],
+  [1, 2, 3],
+];
+
+const rta = matriz.flat(3);
+
+console.log(rta);
